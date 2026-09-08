3d51404eecd6b3d87452c7b839398bc9034a7315
GermanViter <german.viter@icloud.com>
2026-09-08 18:03:46 -0400

    feat(items): completer les classes qui concernent les armes

--- src/item/Weapon.java
+++ src/item/Weapon.java
@@ -1,4 +1,34 @@
 package item;
 
-public class Weapon {
+import dice.Dice;
+
+public class Weapon implements Equipable {
+
+    private final String name;
+    private final Dice damage;
+    private final WeaponCategory category;
+
+    public Weapon(String name, Dice damage, WeaponCategory category) {
+        this.name = name;
+        this.damage = damage;
+        this.category = category;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public Dice getDamage() {
+        return damage;
+    }
+
+    public WeaponCategory getCategory() {
+        return category;
+    }
+
+    @Override
+    public void equip() {
+
+    }
 }
